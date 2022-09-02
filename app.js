d8348724f1bf6ae70fc8e9dfa2c0b0b11c2a543c
escowin <edwin@escowinart.com>
2022-09-02 12:11:13 -0500

    node setup

---
+++ app.js
@@ -0,0 +1,16 @@
+// ** promp questions **
+// meal prep start date?
+// meal prep duration?
+// how many meals?
+// - amount of meal prompts is based on above answer
+// enter food item
+// enter portion size
+// would you like to add another food item?
+//  - if yes, repeat. else continue to next meal
+//  -- next meal repeats previous food item & portion size prompts
+// would you like to add supps?
+// workouts?
+// enter workout
+// enter split
+
+console.log("testing node")
