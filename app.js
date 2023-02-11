221b7268dbd87eb9b32b4fd45434b40b4f6476f8
escowin <edwin@escowinart.com>
2023-02-10 21:43:21 -0600

    fixed init().then() via Promise object return

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const inquirer = require("inquirer");
 // const fs = require('fs');
 
-const prepQuestions = ;
-
 const mealQuestions = [
   {
     type: "input",
@@ -52,9 +50,10 @@ const prepPromptMockData = {
   split: "chest/delts/back/arms/legs",
 };
 
-function init() {
+const init = () => {
   let date = new Date().getFullYear();
-  console.log(`
+  // returns a string via promise object
+  return Promise.resolve(`
   ·················································
   ·                                               ·
   ·               meal prep v0.1.2                ·
@@ -66,69 +65,73 @@ function init() {
 }
 
 const prepPrompt = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "enter prep name:",
-      validate: (nameInput) => {
-        if (!nameInput) {
-          console.log("enter prep name:");
-          return false;
-        }
-        return true;
-      },
-    },
-    {
-      type: "input",
-      name: "startDate",
-      message: "enter start date (yyyy.mm.dd):",
-    },
-    {
-      type: "number",
-      name: "duration",
-      message: "how many weeks?",
-    },
-    {
-      type: "number",
-      name: "meals",
-      message: "how many meals?",
-    },
-    {
-      type: "checkbox",
-      name: "details",
-      message: "select additional details to add:",
-      choices: ["supplements", "cardio", "workout split"],
-    },
-    {
-      type: "input",
-      name: "supps",
-      message: "enter supp:",
-      when: (answers) => answers.details.includes("supplements"),
-    },
-    {
-      type: "input",
-      name: "cardio",
-      message: "enter cardio:",
-      when: (answers) => answers.details.includes("cardio"),
-    },
-    {
-      type: "input",
-      name: "split",
-      message: "enter workout split:",
-      when: (answers) => answers.details.includes("workout split"),
-    },
-  ]);
+  return prepPromptMockData
+  // return inquirer.prompt([
+  //   {
+  //     type: "input",
+  //     name: "name",
+  //     message: "enter prep name:",
+  //     validate: (nameInput) => {
+  //       if (!nameInput) {
+  //         console.log("enter prep name:");
+  //         return false;
+  //       }
+  //       return true;
+  //     },
+  //   },
+  //   {
+  //     type: "input",
+  //     name: "startDate",
+  //     message: "enter start date (yyyy.mm.dd):",
+  //   },
+  //   {
+  //     type: "number",
+  //     name: "duration",
+  //     message: "how many weeks?",
+  //   },
+  //   {
+  //     type: "number",
+  //     name: "meals",
+  //     message: "how many meals?",
+  //   },
+  //   {
+  //     type: "checkbox",
+  //     name: "details",
+  //     message: "select additional details to add:",
+  //     choices: ["supplements", "cardio", "workout split"],
+  //   },
+  //   {
+  //     type: "input",
+  //     name: "supps",
+  //     message: "enter supp:",
+  //     when: (answers) => answers.details.includes("supplements"),
+  //   },
+  //   {
+  //     type: "input",
+  //     name: "cardio",
+  //     message: "enter cardio:",
+  //     when: (answers) => answers.details.includes("cardio"),
+  //   },
+  //   {
+  //     type: "input",
+  //     name: "split",
+  //     message: "enter workout split:",
+  //     when: (answers) => answers.details.includes("workout split"),
+  //   },
+  // ]);
 }
 
 const initMealPrep = (prepInfo) => {
+  console.log(prepInfo)
   if (!prepInfo.mealPrep) {
     prepInfo.mealPrep = [];
   }
 
+  console.log(prepInfo);
+
   return console.log(`
   ·················································
-  ·                  meal ${mealPrep.length + 1} info                  ·
+  ·                  meal # info                  ·
   ·················································
   `);
 }
@@ -145,4 +148,4 @@ function foodPrompt(prepInfo) {
   // });
 }
 // calls
-init().then(prepPrompt).then(foodPrompt)
+init().then(prepPrompt).then(initMealPrep)
