d2740b74ee2e0c644209bef37a99bb04a4e32cce
escowin <edwin@escowinart.com>
2023-02-10 20:52:35 -0600

    chaining .then() methods

--- app.js
+++ app.js
@@ -1,78 +1,52 @@
 const inquirer = require("inquirer");
 // const fs = require('fs');
 
-const prepQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "enter prep name:",
-    validate: (nameInput) => {
-      if (!nameInput) {
-        console.log("enter prep name:");
-        return false;
-      }
-      return true;
-    },
-  },
-  {
-    type: "input",
-    name: "startDate",
-    message: "enter start date (yyyy.mm.dd):",
-  },
-  {
-    type: "number",
-    name: "duration",
-    message: "how many weeks?",
-  },
-  {
-    type: "number",
-    name: "meals",
-    message: "how many meals?",
-  },
-  {
-    type: "checkbox",
-    name: "details",
-    message: "select additional details to add:",
-    choices: ["supplements", "cardio", "workout split"],
-  },
-  {
-    type: "input",
-    name: "supps",
-    message: "enter supp:",
-    when: (answers) => answers.details.includes("supplements"),
-  },
-  {
-    type: "input",
-    name: "cardio",
-    message: "enter cardio:",
-    when: (answers) => answers.details.includes("cardio"),
-  },
-  {
-    type: "input",
-    name: "split",
-    message: "enter workout split:",
-    when: (answers) => answers.details.includes("workout split"),
-  },
-];
+const prepQuestions = ;
 
 const mealQuestions = [
   {
     type: "input",
-    name: "meal",
+    name: "food",
     message: "add food item",
   },
   {
     type: "confirm",
-    name: "continue",
+    name: "confirmAddFood",
     message: "add another food item?",
   },
+  // confirm true repeat prompt
+  // confirm false ends prompt
 ];
 
 const mockData = {
   name: "mock prep",
   startDate: "2023.02.08",
   duration: 3,
-  meals: 4,
+  meals: 3,
+  details: ["cardio", "workout split"],
+  cardio: "rowing/5x week",
+  split: "chest/delts/back/arms/legs",
+  mealPrep: [
+    {
+      food: ["1 egg", "1/2 cup egg whites", "1oz oats"],
+    },
+    {
+      food: ["5oz chicken", "3oz mixed vegies"],
+    },
+    {
+      food: ["1.8oz protein powder, 1 tbsp peanut butter"],
+    },
+    {
+      food: ["6oz white meat", "3oz veggies", "5oz rice"],
+    },
+  ],
+};
+
+const prepPromptMockData = {
+  name: "mock prep",
+  startDate: "2023.02.08",
+  duration: 3,
+  meals: 3,
   details: ["cardio", "workout split"],
   cardio: "rowing/5x week",
   split: "chest/delts/back/arms/legs",
@@ -89,56 +63,86 @@ function init() {
   ·                                               ·
   ·················································
   `);
-
-  // inquirer.prompt(questions).then(answers => {
-  //   for (let i = 0; i < answers.meals;  i++) {
-  //     mealPrompt(answers)
-  //   }
-  // })
-
-  // for (let i = 0; i < mockData.meals; i++) {
-  //   mealPrompt(mockData)
-  // }
-  mealPrompt(mockData);
 }
 
-function mealPrompt(answers, index = 0) {
+const prepPrompt = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "enter prep name:",
+      validate: (nameInput) => {
+        if (!nameInput) {
+          console.log("enter prep name:");
+          return false;
+        }
+        return true;
+      },
+    },
+    {
+      type: "input",
+      name: "startDate",
+      message: "enter start date (yyyy.mm.dd):",
+    },
+    {
+      type: "number",
+      name: "duration",
+      message: "how many weeks?",
+    },
+    {
+      type: "number",
+      name: "meals",
+      message: "how many meals?",
+    },
+    {
+      type: "checkbox",
+      name: "details",
+      message: "select additional details to add:",
+      choices: ["supplements", "cardio", "workout split"],
+    },
+    {
+      type: "input",
+      name: "supps",
+      message: "enter supp:",
+      when: (answers) => answers.details.includes("supplements"),
+    },
+    {
+      type: "input",
+      name: "cardio",
+      message: "enter cardio:",
+      when: (answers) => answers.details.includes("cardio"),
+    },
+    {
+      type: "input",
+      name: "split",
+      message: "enter workout split:",
+      when: (answers) => answers.details.includes("workout split"),
+    },
+  ]);
+}
 
-  // prepInfo object structure
-  // - mealPrep is an array that holds objects equal to the meals key-value.
-  // - each object holds a food array
-  // - the array holds cli string objects
-  // let prepInfo = {
-  //   // other properties
-  //   mealPrep: [
-  //     { 
-  //       food: [ 
-  //         "food item 1",
-  //         "food item 2",
-  //       ],
-  //     },
-  //   ],
-  // };
-  // goal | meal.food
-
-  // fix 
-  // 1 push string values as one object into foodItems array.
-  // 2 foodItems array should be pushed into object
-  // 3 that object needs to be pushed into mealPrepArray
-  if (index >= answers.meals) {
-    console.log(answers);
-    return;
+const initMealPrep = (prepInfo) => {
+  if (!prepInfo.mealPrep) {
+    prepInfo.mealPrep = [];
   }
 
-  inquirer.prompt(mealQuestions).then((mealAnswers) => {
-    answers[`meal${index + 1}`] = mealAnswers.meal;
-    if (mealAnswers.continue) {
-      mealPrompt(answers, index + 1);
-    } else {
-      console.log(answers);
-    }
-  });
+  return console.log(`
+  ·················································
+  ·                  meal ${mealPrep.length + 1} info                  ·
+  ·················································
+  `);
 }
 
+function foodPrompt(prepInfo) {
+  console.log(prepInfo)
+  // inquirer.prompt(mealQuestions).then((answers) => {
+  //   if (!answers.confirmAddFood) {
+  //     foodItems.push(answers.food)
+  //     console.log(foodItems)
+  //   } else {
+  //     foodPrompt();
+  //   }
+  // });
+}
 // calls
-init();
+init().then(prepPrompt).then(foodPrompt)
